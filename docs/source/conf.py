4f1708e905c3a1194d572ac26761eeda4d296423
LucianSpitzner <lucian@s-spitzner.de>
2023-10-04 17:34:17 +0200

    changed sphinx data

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Plasmetheus'
+copyright = '2023, Spitzner'
+author = 'Spitzner'
 
 release = '0.1'
 version = '0.1.0'
