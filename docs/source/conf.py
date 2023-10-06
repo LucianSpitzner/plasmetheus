ebdd588c3bf40feaffe7cd14fb5a7b7cb8ab7f69
LucianSpitzner <lucian@s-spitzner.de>
2023-10-06 13:26:26 +0200

    changed sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'bootstrap-astropy'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
