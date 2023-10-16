07120626cdcffdcbef6f95dd7d0fe47477841f97
LucianSpitzner <lucian@s-spitzner.de>
2023-10-16 17:00:59 +0200

    added icon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,10 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# icon
+
+html_favicon = 'plasIcon.ico'
+
 # -- Options for HTML output
 
 html_theme = 'bootstrap-astropy'
